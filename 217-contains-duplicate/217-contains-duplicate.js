0fc73dc4a2e5cca8f0c46aab4b1a314d2221af4f
Joe Shultz <josephcshultz@gmail.com>
2022-09-14 14:03:45 -0600

    Time: 3649 ms (5.01%), Space: 47.5 MB (99.87%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -5,7 +5,7 @@
 var containsDuplicate = function(nums) {
     let bool=false;
     nums.forEach((num, index)=>{
-        console.log(nums.indexOf(num), index);
+        //console.log(nums.indexOf(num), index);
         if(nums.indexOf(num) !== index){
             bool=true;
         }

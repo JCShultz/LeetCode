8210dbfb9b645b153679cd5616bde7890efbedbb
Joe Shultz <josephcshultz@gmail.com>
2024-01-22 10:39:02 -0700

    Time: 127 ms (89.63%), Space: 58.5 MB (6.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -5,5 +5,6 @@
 var isPalindrome = function(x) {
     var reverseString = x.toString().split('').reverse().join('');
     var string = x.toString();
-    return string === reverseString;
+    //return string === reverseString;
+    return x.toString().split('').reverse().join('') === x.toString();
 };

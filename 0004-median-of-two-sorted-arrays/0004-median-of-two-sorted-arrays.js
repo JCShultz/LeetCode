fa5e8615cb80da2087385745485e451d5ba47980
Joe Shultz <josephcshultz@gmail.com>
2024-04-30 12:59:45 -0600

    Time: 93 ms (74.41%), Space: 56 MB (44.62%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+  let combined = nums1.concat(nums2).sort((a,b)=> a-b);
+  let oddMiddle = combined[Math.floor(combined.length/2)];
+  let evenMiddle = (oddMiddle + combined[Math.floor(combined.length/2)-1])/2;
+
+  if(combined.length % 2 === 1){
+    return oddMiddle;
+  }else{
+    return evenMiddle;
+  }
+
+};

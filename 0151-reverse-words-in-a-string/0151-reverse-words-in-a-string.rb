3027ab7083a6a27c6dab6ac0073ca69efa97008f
Joe Shultz <josephcshultz@gmail.com>
2024-07-22 11:37:43 -0600

    Time: 55 ms (84.58%), Space: 211.8 MB (40.09%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rb
@@ -0,0 +1,8 @@
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+    words = s.split(' ')
+    reverseOrder = words.reverse
+    joinedReverse = reverseOrder.join(' ')
+    return joinedReverse
+end
